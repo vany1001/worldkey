1d5459c38c3b0d4d255eec128457afa165420b87
unknown <vanghan10011999@gmail.com>
2022-09-13 14:14:46 +0700

    hoc

---
+++ Sử dụng toán tử/index.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("nhap chieu rong");
+inputHeight = prompt("nhap chieu cao");
+let Width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = Width * height;
+document.write("Area = " + area);
