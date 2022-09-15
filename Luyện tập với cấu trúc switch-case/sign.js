088d1946a645e0ef18a6840c3bb8f07e0fa847c7
unknown <vanghan10011999@gmail.com>
2022-09-15 22:23:38 +0700

    hoc

---
+++ Luyện tập với cấu trúc switch-case/sign.js
@@ -0,0 +1,11 @@
+let value =prompt('nhap value', 0);
+ switch (value) {
+    case (value > 0) :
+        alert(1);
+        break;
+    case (value < 0) :
+        alert(-1);
+        break;
+    default:  
+        alert(0);
+ }
